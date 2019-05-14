6a7e846c264798843991187b28485436d4aadce2
Aaron Addleman <aaronaddleman@gmail.com>
2019-05-13 19:06:51 -0700

    updates

--- src/hello.clj
+++ src/hello.clj
@@ -1,8 +1,10 @@
 (ns hello
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
+  (:require [clojure.data.json :as json]                                              ;; <1>
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.content-negotiation :as conneg]))                       ;; <2>
 
-(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin"})
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
 
 (defn ok [body]
   {:status 200 :body body})
@@ -13,25 +15,61 @@
 (defn greeting-for [nm]
   (cond
     (unmentionables nm) nil
-    (empty? nm) "Hello, world!\n"
-    :else (str "Hello, " nm "\n")))
+    (empty? nm)         "Hello, world!\n"
+    :else               (str "Hello, " nm "\n")))
 
 (defn respond-hello [request]
-  (let [nm (get-in request [:query-params :name])
+  (let [nm   (get-in request [:query-params :name])
         resp (greeting-for nm)]
     (if resp
       (ok resp)
       (not-found))))
 
+(def echo
+  {:name ::echo
+   :enter #(assoc % :response (ok (:request %)))})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn accepted-type
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  [body content-type]
+  (case content-type
+    "text/html"        body
+    "text/plain"       body
+    "application/edn"  (pr-str body)
+    "application/json" (json/write-str body)))
+
+(defn coerce-to
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
+
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+   (fn [context]
+     (cond-> context
+       (nil? (get-in context [:response :headers "Content-Type"]))              ;; <1>
+       (update-in [:response] coerce-to (accepted-type context))))})                  ;; <2>
+
 (def routes
   (route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]}))
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
+     ["/echo"  :get echo]}))
+
 
 (defn create-server []
-  (http/create-server
-   {::http/routes routes
-    ::http/type   :jetty
-    ::http/port   8890}))
+  (http/create-server                                                                 ;; <1>
+   {::http/routes routes                                                              ;; <2>
+    ::http/type   :jetty                                                              ;; <3>
+    ::http/port   8890}))                                                             ;; <4>
 
 (defn start []
-  (http/start (create-server)))
+  (http/start (create-server)))                                                       ;; <5>
